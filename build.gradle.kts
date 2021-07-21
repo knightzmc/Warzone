a3e4b77a40f64fd2f5886cb0ef3d9a964e45eda1
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-07-21 20:28:43 +0100

    add bungeecord repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ java {
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
 }
 
 dependencies {
