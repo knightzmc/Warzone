53531ae748297bf218cb3e51e0043f56b0cba9d9
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-07-21 22:26:32 +0100

    add qualityarmory dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,10 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://repo.codemc.org/repository/maven-public/") // for qualityarmory
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
+    compileOnly("me.zombie_striker:QualityArmory:1.1.169")
 }
