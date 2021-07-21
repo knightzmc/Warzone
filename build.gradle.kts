c66e62ac3431458643985baf3a393f6fd4e58df5
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-07-21 20:24:29 +0100

    add more boilerplate to gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,17 @@ plugins {
 
 group = "me.bristermitten"
 version = "1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = sourceCompatibility
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
+}
