9413ad543909a11f668a301b399ede5fc5e54669
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-07-25 23:04:38 +0100

    make ratio comparable

--- src/main/java/me/bristermitten/warzone/data/Ratio.java
+++ src/main/java/me/bristermitten/warzone/data/Ratio.java
@@ -1,6 +1,8 @@
 package me.bristermitten.warzone.data;
 
-public record Ratio(int numerator, int denominator) {
+import org.jetbrains.annotations.NotNull;
+
+public record Ratio(int numerator, int denominator) implements Comparable<Ratio> {
 
     public double asDouble() {
         if (denominator == 0) {
@@ -11,5 +13,10 @@ public record Ratio(int numerator, int denominator) {
 
     public String format() {
         return "%d:%d".formatted(numerator, denominator);
+    }
+
+    @Override
+    public int compareTo(@NotNull Ratio o) {
+        return Double.compare(asDouble(), o.asDouble());
     }
 }
