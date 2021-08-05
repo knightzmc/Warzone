a3e2da77c09d264db712384257193de857510dca
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-05 17:42:12 +0100

    fix config

--- src/main/java/me/bristermitten/warzone/leaderboard/menu/LeaderboardMenuConfig.java
+++ src/main/java/me/bristermitten/warzone/leaderboard/menu/LeaderboardMenuConfig.java
@@ -1,12 +1,13 @@
 package me.bristermitten.warzone.leaderboard.menu;
 
 import com.google.gson.annotations.SerializedName;
+import me.bristermitten.warzone.item.ItemConfig;
 import me.bristermitten.warzone.menu.MenuConfig;
 
 import java.util.Map;
 
 public record LeaderboardMenuConfig(
-        @SerializedName("leaderboard-entry") MenuConfig.ItemConfig leaderboardEntry,
+        @SerializedName("leaderboard-entry") ItemConfig leaderboardEntry,
         Map<String, MenuConfig> pages
 ) {
 }
