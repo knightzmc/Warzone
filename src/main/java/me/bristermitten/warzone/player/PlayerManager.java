964a0d5f16d0d1fb45ec187fded5b89a33fb3978
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-11 21:00:59 +0100

    hauhwauhwaiuwhawiu multithreading sucks

--- src/main/java/me/bristermitten/warzone/player/PlayerManager.java
+++ src/main/java/me/bristermitten/warzone/player/PlayerManager.java
@@ -1,9 +1,9 @@
 package me.bristermitten.warzone.player;
 
-import me.bristermitten.warzone.party.PartyManager;
 import me.bristermitten.warzone.player.state.PlayerState;
 import me.bristermitten.warzone.player.state.PlayerStateChangeEvent;
 import me.bristermitten.warzone.player.storage.PlayerStorage;
+import me.bristermitten.warzone.util.Sync;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
@@ -15,23 +15,22 @@ import java.util.function.Consumer;
 public class PlayerManager {
     private final Plugin plugin;
     private final PlayerStorage playerStorage;
-    private final PartyManager partyManager;
 
     @Inject
-    public PlayerManager(Plugin plugin, PlayerStorage playerStorage, PartyManager partyManager) {
+    public PlayerManager(Plugin plugin, PlayerStorage playerStorage) {
         this.plugin = plugin;
         this.playerStorage = playerStorage;
-        this.partyManager = partyManager;
     }
 
-    public boolean setState(WarzonePlayer player, PlayerState newState) {
-        var event = new PlayerStateChangeEvent(player.getCurrentState(), newState, player);
-        Bukkit.getPluginManager().callEvent(event);
-        if (event.isCancelled()) {
-            return false;
-        }
-        player.setCurrentState(newState);
-        return true;
+    public void setState(WarzonePlayer player, PlayerState newState) {
+        Sync.run(() -> {
+            var event = new PlayerStateChangeEvent(player.getCurrentState(), newState, player);
+            Bukkit.getPluginManager().callEvent(event);
+            if (event.isCancelled()) {
+                return;
+            }
+            player.setCurrentState(newState);
+        }, plugin);
     }
 
     public void loadPlayer(@NotNull UUID id, Consumer<WarzonePlayer> onSuccess) {
