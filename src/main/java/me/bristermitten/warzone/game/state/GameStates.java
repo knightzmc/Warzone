a90b7016783d56e5c610461bb1fe49bb543ec2a1
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-13 22:39:57 +0100

    fix states part 3

--- src/main/java/me/bristermitten/warzone/game/state/GameStates.java
+++ src/main/java/me/bristermitten/warzone/game/state/GameStates.java
@@ -3,8 +3,7 @@ package me.bristermitten.warzone.game.state;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-public record GameStates(Provider<InProgressState> inProgressStateProvider, Provider<InLobbyState> inLobbyStateProvider,
-                         Provider<IdlingState> idlingStateProvider) {
+public record GameStates(Provider<InProgressState> inProgressStateProvider, Provider<InLobbyState> inLobbyStateProvider) {
     @Inject
     public GameStates {
     }
