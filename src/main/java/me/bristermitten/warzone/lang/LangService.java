51f386d0d9990a31f28943c0dd435f4e2f8ae6bc
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-24 21:56:58 +0100

    Make LangService work with CommandSender, not just Players

--- src/main/java/me/bristermitten/warzone/lang/LangService.java
+++ src/main/java/me/bristermitten/warzone/lang/LangService.java
@@ -3,8 +3,10 @@ package me.bristermitten.warzone.lang;
 import io.vavr.collection.HashMap;
 import io.vavr.control.Option;
 import me.bristermitten.warzone.chat.ChatFormatter;
+import me.bristermitten.warzone.util.Cast;
 import net.kyori.adventure.title.Title;
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
@@ -23,7 +25,7 @@ public class LangService {
         this.configProvider = configProvider;
     }
 
-    public void send(@NotNull Player receiver, LangElement message, @NotNull Map<String, Object> placeholders) {
+    public void send(@NotNull CommandSender receiver, LangElement message, @NotNull Map<String, Object> placeholders) {
         UnaryOperator<String> applyPlaceholders = str -> HashMap.ofAll(placeholders)
                 .foldLeft(str, (s, pattern) -> s.replace(pattern._1(), pattern._2.toString()));
 
@@ -41,21 +43,21 @@ public class LangService {
         }
     }
 
-    public void sendMessage(@NotNull Player receiver, @NotNull Function<LangConfig, LangElement> message) {
+    public void sendMessage(@NotNull CommandSender receiver, @NotNull Function<LangConfig, LangElement> message) {
         sendMessage(receiver, message, Map.of());
     }
 
-    public void sendMessage(@NotNull Player receiver, @NotNull Function<LangConfig, LangElement> message, @NotNull Map<String, Object> placeholders) {
+    public void sendMessage(@NotNull CommandSender receiver, @NotNull Function<LangConfig, LangElement> message, @NotNull Map<String, Object> placeholders) {
         send(receiver, message.apply(configProvider.get()), placeholders);
     }
 
-    public void sendMessage(@NotNull Player receiver, @NotNull String message) {
-        receiver.sendMessage(formatter.format(message, receiver));
+    public void sendMessage(@NotNull CommandSender receiver, @NotNull String message) {
+        receiver.sendMessage(formatter.format(message, Cast.safe(receiver, OfflinePlayer.class)));
     }
 
-    public void sendTitle(@NotNull Player receiver, @NotNull String title, @NotNull String subtitle) {
-        var formattedTitle = formatter.format(title, receiver);
-        var formattedSubtitle = formatter.format(subtitle, receiver);
+    public void sendTitle(@NotNull CommandSender receiver, @NotNull String title, @NotNull String subtitle) {
+        var formattedTitle = formatter.format(title, Cast.safe(receiver, OfflinePlayer.class));
+        var formattedSubtitle = formatter.format(subtitle, Cast.safe(receiver, OfflinePlayer.class));
         receiver.showTitle(Title.title(formattedTitle, formattedSubtitle));
     }
 }
