eddd9406da21105c12ca0e50f405c6c6b61983aa
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-28 16:37:55 +0100

    fix commandmodule multibinding

--- src/main/java/me/bristermitten/warzone/commands/CommandsModule.java
+++ src/main/java/me/bristermitten/warzone/commands/CommandsModule.java
@@ -3,7 +3,6 @@ package me.bristermitten.warzone.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.PaperCommandManager;
 import com.google.inject.AbstractModule;
-import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
 import me.bristermitten.warzone.commands.args.*;
@@ -23,12 +22,12 @@ public class CommandsModule extends AbstractModule {
         commandBinder.addBinding().to(PartyCommand.class);
         commandBinder.addBinding().to(LeaderboardCommand.class);
 
-        var argProcessorBinder = Multibinder.newSetBinder(binder(), Key.get(new TypeLiteral<ArgumentProcessor<?>>() {
-        }.getRawType()));
+        var argProcessorBinder = Multibinder.newSetBinder(binder(), new TypeLiteral<ArgumentProcessor<?>>() {
+        });
         argProcessorBinder.addBinding().to(ArenaArgumentProcessor.class);
 
-        var conditionBinder = Multibinder.newSetBinder(binder(), Key.get(new TypeLiteral<ArgumentCondition<?>>() {
-        }.getRawType()));
+        var conditionBinder = Multibinder.newSetBinder(binder(), new TypeLiteral<ArgumentCondition<?>>() {
+        });
         conditionBinder.addBinding().to(FreeArenaCondition.class);
         conditionBinder.addBinding().to(InUseArenaCondition.class);
     }
