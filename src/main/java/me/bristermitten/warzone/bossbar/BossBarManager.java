85ea59963a9d4f6902937f3002c1c82d339ba7ce
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-30 15:07:41 +0100

    fix BossBarManager not compiling somehow

--- src/main/java/me/bristermitten/warzone/bossbar/BossBarManager.java
+++ src/main/java/me/bristermitten/warzone/bossbar/BossBarManager.java
@@ -1,13 +1,12 @@
 package me.bristermitten.warzone.bossbar;
 
-import me.bristermitten.warzone.bossbar.game.GameBossBar;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public interface BossBarManager {
     /**
-     * Renders a given {@link GameBossBar} and shows it to a given {@link Player}
+     * Renders a given {@link CustomBossBar} and shows it to a given {@link Player}
      * If the player is already viewing bossBar, nothing will happen.
      * <p>
      * This will keep the boss bar visible until
@@ -15,7 +14,7 @@ public interface BossBarManager {
     void show(UUID player, CustomBossBar bossBar);
 
     /**
-     * Hides a given {@link GameBossBar} from a given {@link Player}
+     * Hides a given {@link CustomBossBar} from a given {@link Player}
      */
     void hide(UUID player, CustomBossBar bossBar);
 
