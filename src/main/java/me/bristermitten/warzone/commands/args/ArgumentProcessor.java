70f65b4ab0a1390de76e1068fbaec06078de4e90
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-01 20:32:04 +0100

    remove redundant interface inheritance

--- src/main/java/me/bristermitten/warzone/commands/args/ArgumentProcessor.java
+++ src/main/java/me/bristermitten/warzone/commands/args/ArgumentProcessor.java
@@ -1,7 +1,4 @@
 package me.bristermitten.warzone.commands.args;
 
-import co.aikar.commands.BukkitCommandExecutionContext;
-import co.aikar.commands.contexts.ContextResolver;
-
-public interface ArgumentProcessor<T> extends TabCompleter, ArgumentContext<T>, ContextResolver<T, BukkitCommandExecutionContext> {
+public interface ArgumentProcessor<T> extends TabCompleter, ArgumentContext<T> {
 }
