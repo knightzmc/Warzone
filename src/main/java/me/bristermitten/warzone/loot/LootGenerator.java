21e5bd8ca7fee51ab77119b3b977e73809e5a338
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-09 22:13:40 +0100

    LootGenerator doesn't need to be a singleton

--- src/main/java/me/bristermitten/warzone/loot/LootGenerator.java
+++ src/main/java/me/bristermitten/warzone/loot/LootGenerator.java
@@ -1,12 +1,10 @@
 package me.bristermitten.warzone.loot;
 
-import com.google.inject.Singleton;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Singleton
 public class LootGenerator {
 
     public Collection<ItemStack> generateLoot(LootTable table) {
