abb1ddc9fd597c0a06d2e2f898c6f2fd49d28a44
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-09 20:53:26 +0100

    fix barriers in offhand and armour slots

--- src/main/java/me/bristermitten/warzone/player/PlayerInventoryBlocker.java
+++ src/main/java/me/bristermitten/warzone/player/PlayerInventoryBlocker.java
@@ -13,19 +13,30 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Set;
+import java.util.UUID;
+
 public class PlayerInventoryBlocker implements EventListener {
     private static final ItemStack BARRIER_ITEM = new ItemStack(Material.BARRIER);
     private static final NamespacedKey BARRIER_KEY = new NamespacedKey(JavaPlugin.getPlugin(Warzone.class), "inventory_barrier");
     private static final int FIRST_NON_HOTBAR_SLOT = 9;
+    private static final Set<Integer> BLOCKED_SLOTS = Set.of(36, 37, 38, 39, 40);
 
-    static {
-        BARRIER_ITEM.editMeta(meta -> meta.getPersistentDataContainer().set(BARRIER_KEY, PersistentDataType.BYTE, (byte) 1));
+    private static ItemStack createBarrierItem() {
+        var item = BARRIER_ITEM.clone();
+        // Make every item unique
+        item.editMeta(meta ->
+                meta.getPersistentDataContainer().set(BARRIER_KEY, PersistentDataType.STRING, UUID.randomUUID().toString()));
+        return item;
     }
 
     public void blockInventory(Player player) {
         var inventory = player.getInventory();
         for (int i = FIRST_NON_HOTBAR_SLOT; i < inventory.getSize(); i++) {
-            inventory.setItem(i, BARRIER_ITEM.clone());
+            if (BLOCKED_SLOTS.contains(i)) {
+                continue;
+            }
+            inventory.setItem(i, createBarrierItem());
         }
     }
 
@@ -42,7 +53,7 @@ public class PlayerInventoryBlocker implements EventListener {
         if (item == null) {
             return;
         }
-        if (item.hasItemMeta() && item.getItemMeta().getPersistentDataContainer().has(BARRIER_KEY, PersistentDataType.BYTE)) {
+        if (item.hasItemMeta() && item.getItemMeta().getPersistentDataContainer().has(BARRIER_KEY, PersistentDataType.STRING)) {
             event.setCancelled(true);
         }
     }
