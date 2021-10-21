84538416b25b107c23470bc06ee2efdc6f698183
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-21 23:37:46 +0100

    make players invulnerable when they're spawning

--- src/main/java/me/bristermitten/warzone/player/state/game/InGameSpawningState.java
+++ src/main/java/me/bristermitten/warzone/player/state/game/InGameSpawningState.java
@@ -3,7 +3,9 @@ package me.bristermitten.warzone.player.state.game;
 import me.bristermitten.warzone.bossbar.BossBarManager;
 import me.bristermitten.warzone.chat.channel.ChatChannel;
 import me.bristermitten.warzone.game.repository.GameRepository;
+import me.bristermitten.warzone.player.WarzonePlayer;
 import me.bristermitten.warzone.scoreboard.ScoreboardManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,5 +18,15 @@ public class InGameSpawningState extends InGameState {
     @Inject
     InGameSpawningState(ScoreboardManager scoreboardManager, @Named("inGame") ChatChannel channel, BossBarManager bossBarManager, GameRepository gameRepository) {
         super(scoreboardManager, channel, bossBarManager, gameRepository);
+    }
+
+    @Override
+    public void onEnter(@NotNull WarzonePlayer player) {
+        player.getPlayer().peek(p -> p.setInvulnerable(true));
+    }
+
+    @Override
+    public void onLeave(@NotNull WarzonePlayer player) {
+        player.getPlayer().peek(p -> p.setInvulnerable(false));
     }
 }
